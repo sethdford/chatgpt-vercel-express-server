61d72ac6335190580ce6f9182334d88d175668c6
Seth Ford <sethford@Seths-MacBook-Air.local>
2023-01-23 14:52:08 -0500

    updates to root

--- index.js
+++ index.js
@@ -6,5 +6,11 @@ app.use(express.json({ extended: false }));
 
 app.use("/api/product", product);
 
+app.get('/', async (req, res) => {
+    res.status(200).send({
+      message: 'Hello from chat-gpt-express-server !!'
+    })
+  })
+
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
