aebf6fcd8436eed17c5e14bc277da96c4adf121b
Seth Ford <sethford@Seths-MacBook-Air.local>
2023-01-23 18:39:34 -0500

    updates to the server

--- index.js
+++ index.js
@@ -25,5 +25,29 @@ app.get('/', async (req, res) => {
     })
   })
 
+  app.post('/', async (req, res) => {
+    try {
+      const prompt = req.body.prompt;
+  
+      const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: `${prompt}`, // The prompt is the text that the model will use to generate a response.
+        temperature: 0, // Higher values means the model will take more risks.
+        max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
+        top_p: 1, // alternative to sampling with temperature, called nucleus sampling
+        frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
+        presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+      });
+  
+      res.status(200).send({
+        bot: response.data.choices[0].text
+      });
+  
+    } catch (error) {
+      console.error(error)
+      res.status(500).send(error || 'Sorry, something went wrong. Please try again later.');
+    }
+  })
+  
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
